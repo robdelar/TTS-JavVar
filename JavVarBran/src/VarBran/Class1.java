bbd5226571564f3da5d2885b082260b3efd35e5e
robdelar <44275659+robdelar@users.noreply.github.com>
2022-07-20 21:52:32 -0700

    Add files via upload

---
+++ JavVarBran/src/VarBran/Class1.java
@@ -0,0 +1,19 @@
+package VarBran;
+
+public class Class1 {
+	
+	public static void main(String[] args) {
+		int num1;
+		String name = "Robert";
+		double num2;
+		String 8name = "Bob";
+		
+		System.out.println(num1);
+		System.out.println(name);
+		System.out.println(num2);
+		System.out.println(8name);
+		
+		
+	}
+
+}
